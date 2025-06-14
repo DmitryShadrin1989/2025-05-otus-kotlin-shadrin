ed4b3567ffa63243175382712011667c674be548
DmitryShadrin <dmitry.shadrin.alex1989@gmail.com>
2025-06-14 22:14:42 +0300

    The repository has been reorganized into a multi-module one + the first module has been added

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "2025-05-otus-kotlin-shadrin"
+
+include("hw-1")
